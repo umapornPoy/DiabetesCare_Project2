a1d0f5ae03d227e29a1311e040c4c0356fefabfb
umapornPoy <p.tunlang7@gmail.com>
2017-02-22 17:39:07 +0700

    update mongodb

--- DiabetesProject2/DiabetesProject2/DiabetesJS/app.js
+++ DiabetesProject2/DiabetesProject2/DiabetesJS/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('Diabetes', ['ngRoute','ui.bootstrap']);
+﻿var app = angular.module('Diabetes', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
 
@@ -35,6 +35,10 @@ app.config(['$routeProvider', function ($routeProvider) {
              templateUrl: 'Nutrition/Nutrients',
              controller: 'NutrientsController'
          })
+       /* .when('/Register', {
+            templateUrl: 'Register/Register',
+            controller: 'RegisterController'
+        })  */
      .otherwise({
          templateUrl: '/'
      })
---
+++ DiabetesProject2/DiabetesProject2/controller/Register.js
@@ -0,0 +1,49 @@
+﻿
+app.controller('RegisterController', ['$scope', '$http', function ($scope, $http) {
+    console.log("Hello World from controller");
+
+
+    var refresh = function () {
+        $http.get('/diabetesmember').success(function (response) {
+            console.log("I got the data I requested");
+            $scope.diabetesmember = response;
+            $scope.contact = "";
+        });
+    };
+
+    refresh();
+
+    $scope.addContact = function () {
+        console.log($scope.contact);
+        $http.post('/diabetesmember', $scope.contact).success(function (response) {
+            console.log(response);
+            refresh();
+        });
+    };
+
+    $scope.remove = function (id) {
+        console.log(id);
+        $http.delete('/diabetesmember/' + id).success(function (response) {
+            refresh();
+        });
+    };
+
+    $scope.edit = function (id) {
+        console.log(id);
+        $http.get('/diabetesmember/' + id).success(function (response) {
+            $scope.contact = response;
+        });
+    };
+
+    $scope.update = function () {
+        console.log($scope.contact._id);
+        $http.put('/diabetesmember/' + $scope.contact._id, $scope.contact).success(function (response) {
+            refresh();
+        })
+    };
+
+    $scope.deselect = function () {
+        $scope.contact = "";
+    }
+
+}]);
